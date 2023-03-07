6a0a7166c6560ba0f6cb5bab72ae6c2908259730
jeremyHzn <heizmann.jeremy@gmail.com>
2023-03-07 11:50:36 +0100

    exo4

--- assets/scripts/accordeon.js
+++ assets/scripts/accordeon.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-    
+    // Selection
+var titre1 = $('#titre1');
+var titre2 = $('#titre2');
+var titre3 = $('#titre3');
+
+var contenu1 = $('#contenu1');
+var contenu2 = $('#contenu2');
+var contenu3 = $('#contenu3');
 });
--- assets/scripts/couleurs.js
+++ assets/scripts/couleurs.js
@@ -1,3 +1,42 @@
 $(document).ready(function () {
+    // Selection des éléments
+    var container = $('#containerColor');
+    var clicColor = $('#clicColor');
+    var affichRed = $('#affichRed');
+    var affichGreen = $('#affichGreen');
+    var affichBlue = $('#affichBlue');
     
-});
+    $(clicColor).click(function () { 
+        var red = Math.floor(Math.random() * 256);
+        var green = Math.floor(Math.random() * 256);
+        var blue = Math.floor(Math.random() * 256);
+        affichRed.value = red;
+        affichGreen.value = green;
+        affichBlue.value = blue;
+        
+        $(container).css("background-color", 'rgb(' + red + ',' + green + ',' + blue + ')');
+        
+        // Partie 2
+        var formColor = $('#formColor');
+        var messError = $('#error');
+        
+        $(formColor).click(function () { 
+            var red = affichRed.value;
+            var green = affichGreen.value;
+            var blue = affichBlue.value;
+            console.log(typeof red)
+            
+            if (parseInt(red) >= 0 && parseInt(red) <= 255 && parseInt(green) >= 0 && parseInt(green) <= 255 && parseInt(blue) >= 0 && parseInt(blue) <= 255 ) {
+                $(container).css("background-color", 'rgb(' + red + ',' + green + ',' + blue + ')');
+            }
+            else{
+                console.log('Erreur de sélection de couleur');
+                messError.classList.remove('invisible');
+                setTimeout(function() {
+                    messError.classList.add('invisible');
+                },3000);
+            };
+            // accolades pas bien mises je sais  
+        };
+    }
+}
